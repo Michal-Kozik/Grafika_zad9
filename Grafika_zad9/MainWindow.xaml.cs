cc5577f76c057c5a09b9815ad38c59c631460836
Michal Kozik <michal.kozikowski99@gmail.com>
2021-12-06 18:12:31 +0100

    Prowizorycznie działanie wykrywania dowolnego koloru

--- Grafika_zad9/MainWindow.xaml.cs
+++ Grafika_zad9/MainWindow.xaml.cs
@@ -136,14 +136,14 @@ namespace Grafika_zad9
                     other++;
                 }
             }
-            double r = Math.Round(redField / (redField + greenField + blueField + other), 2) * 100;
-            redLabel.Content = $"Tereny czerwone: {r}%";
-            double g = Math.Round(greenField / (redField + greenField + blueField + other), 2) * 100;
-            greenLabel.Content = $"Tereny zielone: {g}%";
-            double b = Math.Round(blueField / (redField + greenField + blueField + other), 2) * 100;
-            blueLabel.Content = $"Tereny niebieskie: {b}%";
-            double others = Math.Round(other / (redField + greenField + blueField + other), 2) * 100;
-            otherLabel.Content = $"Tereny inne: {others}%";
+            double r = Math.Round(redField / (redField + greenField + blueField + other), 5) * 100;
+            redLabel.Content = $"Czerwone: {r}%";
+            double g = Math.Round(greenField / (redField + greenField + blueField + other), 5) * 100;
+            greenLabel.Content = $"Zielone: {g}%";
+            double b = Math.Round(blueField / (redField + greenField + blueField + other), 5) * 100;
+            blueLabel.Content = $"Niebieskie: {b}%";
+            double others = Math.Round(other / (redField + greenField + blueField + other), 5) * 100;
+            otherLabel.Content = $"Inne: {others}%";
             CreateChart(r, g, b, others);
 
             // Rezultat.
@@ -155,6 +155,90 @@ namespace Grafika_zad9
             Marshal.Copy(pixelBufferResult, 0, resultBitmapData.Scan0, pixelBufferResult.Length);
             imgResultBitmap.UnlockBits(resultBitmapData);
             imgResult.Source = ConvertBitmapToImageSource(imgResultBitmap);
+        }
+
+        private void CustomAnalize(object sender, RoutedEventArgs e)
+        {
+            // TODO: walidacja.
+            Bitmap imgSourceBitmap = ConvertImgToBitmap(imgSource);
+            BitmapData sourceBitmapData = imgSourceBitmap.LockBits(new System.Drawing.Rectangle(0, 0, imgSourceBitmap.Width, imgSourceBitmap.Height),
+                                                            ImageLockMode.ReadOnly,
+                                                            System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            byte[] pixelBuffer = new byte[sourceBitmapData.Stride * sourceBitmapData.Height];
+            Marshal.Copy(sourceBitmapData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+
+            byte[] pixelBufferResult = new byte[sourceBitmapData.Stride * sourceBitmapData.Height];
+            Marshal.Copy(sourceBitmapData.Scan0, pixelBufferResult, 0, pixelBufferResult.Length);
+            imgSourceBitmap.UnlockBits(sourceBitmapData);
+
+            byte redFromInput = Convert.ToByte(redFrom.Text);
+            byte greenFromInput = Convert.ToByte(greenFrom.Text);
+            byte blueFromInput = Convert.ToByte(blueFrom.Text);
+            byte redToInput = Convert.ToByte(redTo.Text);
+            byte greenToInput = Convert.ToByte(greenTo.Text);
+            byte blueToInput = Convert.ToByte(blueTo.Text);
+
+            double inRange = 0;
+            double outOfRange = 0;
+            // Analiza obrazu.
+            for (int i = 0; i + 4 < pixelBuffer.Length; i += 4)
+            {
+                // Tereny in range.
+                if (pixelBuffer[i] >= blueFromInput && pixelBuffer[i] <= blueToInput 
+                && pixelBuffer[i + 1] >= greenFromInput && pixelBuffer[i + 1] <= greenToInput
+                && pixelBuffer[i + 2] >= redFromInput && pixelBuffer[i + 2] <= redToInput)
+                {
+                    pixelBufferResult[i] = 0;
+                    pixelBufferResult[i + 1] = 255;
+                    pixelBufferResult[i + 2] = 255;
+                    inRange++;
+                }
+                // Tereny out of range.
+                else
+                {
+                    pixelBufferResult[i] = 0;
+                    pixelBufferResult[i + 1] = 0;
+                    pixelBufferResult[i + 2] = 0;
+                    outOfRange++;
+                }
+            }
+            double inRangePercent = Math.Round(inRange / (inRange + outOfRange), 5) * 100;
+            inRangeLabel.Content = $"Należy: {inRangePercent}%";
+            double outOfRangePercent = Math.Round(outOfRange / (inRange + outOfRange), 5) * 100;
+            outOfRangeLabel.Content = $"Nie należy: {outOfRangePercent}%";
+            CreateCustomChart(inRangePercent, outOfRangePercent);
+
+            // Rezultat.
+            Bitmap imgResultBitmap = new Bitmap(imgSourceBitmap.Width, imgSourceBitmap.Height);
+            BitmapData resultBitmapData = imgResultBitmap.LockBits(new System.Drawing.Rectangle(0, 0, imgResultBitmap.Width, imgResultBitmap.Height),
+                                                            ImageLockMode.WriteOnly,
+                                                            System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            Marshal.Copy(pixelBufferResult, 0, resultBitmapData.Scan0, pixelBufferResult.Length);
+            imgResultBitmap.UnlockBits(resultBitmapData);
+            imgResult.Source = ConvertBitmapToImageSource(imgResultBitmap);
+        }
+
+        private void CreateCustomChart(double inRangePercent, double outOfRangePercent)
+        {
+            customStatistics.Children.Clear();
+            for (int i = 0; i < inRangePercent; i++)
+            {
+                System.Windows.Shapes.Rectangle rectangle = new System.Windows.Shapes.Rectangle();
+                rectangle.Width = 10;
+                rectangle.Height = 1;
+                rectangle.Fill = new SolidColorBrush(Colors.Orange);
+                customStatistics.Children.Add(rectangle);
+            }
+            for (int i = 0; i < outOfRangePercent; i++)
+            {
+                System.Windows.Shapes.Rectangle rectangle = new System.Windows.Shapes.Rectangle();
+                rectangle.Width = 10;
+                rectangle.Height = 1;
+                rectangle.Fill = new SolidColorBrush(Colors.Gray);
+                customStatistics.Children.Add(rectangle);
+            }
         }
 
         private void CreateChart(double r, double g, double b, double others)
@@ -192,6 +276,20 @@ namespace Grafika_zad9
                 rectangle.Fill = new SolidColorBrush(Colors.Gray);
                 statistics.Children.Add(rectangle);
             }
+        }
+
+        private void ShowColor(object sender, RoutedEventArgs e)
+        {
+            //TODO: Walidacja
+            byte redFromInput = Convert.ToByte(redFrom.Text);
+            byte greenFromInput = Convert.ToByte(greenFrom.Text);
+            byte blueFromInput = Convert.ToByte(blueFrom.Text);
+            byte redToInput = Convert.ToByte(redTo.Text);
+            byte greenToInput = Convert.ToByte(greenTo.Text);
+            byte blueToInput = Convert.ToByte(blueTo.Text);
+
+            colorFrom.Fill = new SolidColorBrush(System.Windows.Media.Color.FromRgb(redFromInput, greenFromInput, blueFromInput));
+            colorTo.Fill = new SolidColorBrush(System.Windows.Media.Color.FromRgb(redToInput, greenToInput, blueToInput));
         }
 
         private Bitmap ConvertImgToBitmap(System.Windows.Controls.Image source)
